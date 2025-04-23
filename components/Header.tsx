07c079ad8a6d9181c4ede29ab9c2f6b5abb26afe
hrithik210 <hrithiknayak7829@gmail.com>
2025-04-23 16:59:07 +0530

    added header component

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,6 +2,8 @@
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { Button } from "./ui/button";
+import { ShoppingCart, User } from "lucide-react";
 
 
 export default function Header(){
@@ -9,16 +11,64 @@ export default function Header(){
     const isAdmin = pathname.startsWith("/admin")
 
     return (
-        <div className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur
+        <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur
         supports-[backdrop-filter]:bg-background/60">
             <div className="container flex h-16 items-center justify-between">
-                <div className="flex items-center gap0-6 md:gap-6">
+                <div className="flex items-center gap-6 md:gap-10">
                     <Link href='/' className="flex items-center space-x-2 ">
                         <span className="text-xl font-bold p-4">NiggaByte</span>
                     </Link>
+
+                    <div className="hidden md:flex flex-1 justify-center">
+                        <nav className="hidden md:flex gap-6">
+                            <Link href='/' className="text-sm font-medium
+                            transition-colors hover:text-primary">
+                                Home
+                            </Link>
+                        
+                            <Link href='/' className="text-sm font-medium
+                            transition-colors hover:text-primary">
+                                Products 
+                            </Link>
+                            
+                            <Link href='/' className="text-sm font-medium
+                            transition-colors hover:text-primary">
+                                Categories
+                            </Link>
+                            
+                            <Link href='/' className="text-sm font-medium
+                            transition-colors hover:text-primary">
+                                About
+                            </Link>
+                        </nav>
+                    </div>
                 </div>
 
+                {!isAdmin && (
+                    <div className="flex items-center gap-6">
+                        <Button variant='ghost'>
+                            <ShoppingCart className="h-5 w-6" size='icon'/>
+                            <span className="sr-only">Cart</span>
+                        </Button>
+                        
+                        <Button variant='ghost'>
+                            <User  className="h-6 w-6" size='icon'/>
+                            <span className="sr-only">User Account</span>
+                        </Button>
+                    </div>
+                )}
+
+                {isAdmin && (
+                    <div className="flex items-center  gap-6">
+                        <Link href='/admin'>
+                            <Button variant='outline' size='sm'>
+                                Admin Dashboard
+                            </Button>
+                        </Link>
+                    </div>
+                )}
+
             </div>
-        </div>
+        </header>
     )
 }
