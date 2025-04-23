c29d018082ffc135e2a8f28a9d838945ce54fd3e
hrithik210 <hrithiknayak7829@gmail.com>
2025-04-24 00:00:01 +0530

    init maincard

---
+++ components/mainCard.tsx
@@ -0,0 +1,14 @@
+import { Card } from "./ui/card";
+
+export default function MainCard(){
+    return (
+        <Card className="overflow-hidden border-0 shadow-lg">
+            <div className="grid grid-cols-1 gap-6">
+                <div>
+                    <Image 
+                    />
+                </div>
+            </div>
+        </Card>
+    )
+}
