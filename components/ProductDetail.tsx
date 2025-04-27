cb13210ad2a88ff8cc751fb63ba283eb90725009
hrithik210 <hrithiknayak7829@gmail.com>
2025-04-27 23:56:34 +0530

    layout for product

--- components/ProductDetail.tsx
+++ components/ProductDetail.tsx
@@ -1,4 +1,5 @@
 import { Product } from "@/lib/types"
+import { useState } from "react"
 
 
 interface ProductDetailProps{
@@ -6,8 +7,26 @@ interface ProductDetailProps{
 }
 
 export default function ProductDetail({product} : ProductDetailProps){
+  const [currentIndex , setCurrentIndex] = useState(0);
   return (
-    <div>
+    <div className="grid md:grid-cols-2 gap-8 lg:gap-16">
+      
+      {/* left side */}
+      <div className="relative">
+        <div className="relative aspect-square overflow-hidden rounded-lg border"
+          {...swipeHandlers}
+          aria-live="polite">
+        </div>
+      </div>
+
+      {/* right side */}
+
+      <div className="space y-4">
+        <div className="">
+          <h1>Product Name</h1>
+          <div>Product Ratings and reviews</div>
+        </div>
+      </div>
 
     </div>
   )
