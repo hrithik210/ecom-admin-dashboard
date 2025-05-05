efacaf15a380ef8fd83f2d18eb7047a2a707bf75
hrithik210 <hrithiknayak7829@gmail.com>
2025-05-05 23:52:27 +0530

    some changes

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,8 +1,20 @@
+import { Button } from "@/components/ui/button";
+import { Plus } from "lucide-react";
+import Link from "next/link";
 
 
 export default function AdminDashboard(){
   return (
     <div>
+      <div>
+        <h1>Product Management</h1>
+        <Button>
+          <Link href="/admin/products/add">
+            <Plus className="mr-2 h-4 w-4"/>
+            Add Product
+          </Link>
+        </Button>
+      </div>
     </div>
   )
 }
