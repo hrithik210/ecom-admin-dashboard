8eeed9b65821a57d3d244619499d816ff6c75ab2
hrithik210 <hrithiknayak7829@gmail.com>
2025-05-06 23:48:56 +0530

    added some routes

--- components/admin/AdminSidebar.tsx
+++ components/admin/AdminSidebar.tsx
@@ -1,6 +1,38 @@
+import { BarChart3, Package, Settings, ShoppingBag, Users } from "lucide-react"
+import { usePathname } from "next/navigation"
 
+
+const items = [
+  {
+    name: "Dashboard",
+    href: "/admin",
+    icon: BarChart3,
+  },
+  {
+    name: "Products",
+    href: "/admin/products",
+    icon: Package,
+  },
+  {
+    name: "Orders",
+    href: "/admin/orders",
+    icon: ShoppingBag,
+  },
+  {
+    name: "Customers",
+    href: "/admin/customers",
+    icon: Users,
+  },
+  {
+    name: "Settings",
+    href: "/admin/settings",
+    icon: Settings,
+  },
+]
 
 const AdminSidebar = () => {
+  const pathname = usePathname();
+  
   return (
     <div>AdminSidebar</div>
   )
