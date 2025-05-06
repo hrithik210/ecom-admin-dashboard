137c87ad1fa0d99ce0a9e7acec03b5ae5678cb82
hrithik210 <hrithiknayak7829@gmail.com>
2025-05-07 00:07:50 +0530

    made admin sidebar full fledged

--- components/admin/AdminSidebar.tsx
+++ components/admin/AdminSidebar.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import { cn } from "@/lib/utils";
 import { BarChart3, Package, Settings, ShoppingBag, Users } from "lucide-react"
 import Link from "next/link";
 import { usePathname } from "next/navigation"
@@ -43,6 +44,29 @@ const AdminSidebar = () => {
           <Package />
           <span>Admin panel</span>
         </Link>
+      </div>
+
+      <div className="flex-1 overflow-auto py-2">
+        <nav className="grid items-start text-sm font-medium px-2">
+          {items.map((item) => {
+            const isActive = pathname === item.href
+            return (
+              <Link 
+                key={item.href}
+                href={item.href}
+                className={cn(
+                  "flex items-center gap-3 rounded-lg px-3 py-2 transition-all",
+                  isActive
+                    ? "bg-primary text-primary-foreground"
+                    : "text-muted-foreground hover:bg-muted hover:text-foreground",
+                )}>
+                  <item.icon className="h-4 w-4" />
+                  <span>{item.name}</span>
+              </Link>
+            )
+          })}
+        </nav>
+
       </div>
     </div>
   )
