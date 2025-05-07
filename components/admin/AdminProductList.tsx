1e449ce2aca5da29e02ab8ff2d5595ede41b68bf
hrithik210 <hrithiknayak7829@gmail.com>
2025-05-07 23:46:58 +0530

    admin prodlist init changes

--- components/admin/AdminProductList.tsx
+++ components/admin/AdminProductList.tsx
@@ -1,13 +1,21 @@
 import { Product } from '@/lib/types'
-import React from 'react'
+import React, { useState } from 'react'
+import { Card } from '../ui/card'
 
 interface AdminProductListProps {
   products : Product[]
 }
 
 const AdminProductList = ({products} : AdminProductListProps) => {
+  const [productList , setProductList] = useState<Product[]>(products)
   return (
-    <div>AdminProductList</div>
+    <div className='grid gap-6 md:grid-cols-2 lg:grid-cols-3'>
+      {productList.map((product) => (
+        <Card>
+          
+        </Card>
+      ))}
+    </div>
   )
 }
 
