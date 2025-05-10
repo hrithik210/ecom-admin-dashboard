0cec439f109630e6057e53818f72717f7328e521
hrithik210 <hrithiknayak7829@gmail.com>
2025-05-10 23:57:44 +0530

    some changes

--- components/admin/AddProductForm.tsx
+++ components/admin/AddProductForm.tsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import { Product } from '@/lib/types'
+import React, { useState } from 'react'
+import { Card } from '../ui/card'
 
 const AddProductForm = () => {
+  const [formData, setFormData] = useState<Partial<Product>>({
+    name: "",
+    description: "",
+    price: 0,
+    images: [""],
+    category: "",
+    colors: [],
+    sizes: [],
+    isMainCard: false,
+    reviews: [],
+    rating: 0,
+  })
   return (
-    <div>AddProductForm</div>
+    <form>
+      <div className='grid gap-6 mb-6'>
+        <Card>
+          
+        </Card>
+      </div>
+    </form>
   )
 }
 
