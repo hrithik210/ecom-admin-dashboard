28c5265ebf968ba16d27a849da8be3d2ccd94c77
hrithik210 <hrithiknayak7829@gmail.com>
2025-05-14 23:40:54 +0530

    added all products page

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,8 +1,19 @@
+import ProductGrid from '@/components/ProductGrid';
+import { getProductById } from '@/lib/Products';
 import React from 'react'
 
 const AllProductsRender = () => {
+
+const product =  getProductById("1")
+  
+if(!product){
+    return <div>Product not found</div>
+}
+
   return (
-    <div>page</div>
+    <div>
+        <ProductGrid products={[product]} />
+    </div>
   )
 }
 
